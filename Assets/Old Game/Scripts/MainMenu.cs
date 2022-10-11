e07dd4c4fde67d88d06d10b931524c7d1c9a0db7
MaxElam2021 <94044736+MaxElam2021@users.noreply.github.com>
2022-10-11 15:32:50 +0100

    CUTSCENE ADDED

---
+++ Assets/Old Game/Scripts/MainMenu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public AudioSource sfxPlayer;
+
+    void Start()
+    {
+        sfxPlayer = GetComponent<AudioSource>();
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+	public void HowToPlay()
+    {
+        SceneManager.LoadScene("HowToPlay");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
