d8083caa9c7789beb9d9bf4e39ba551efa5ce1b7
Tguedes2021 <94044814+Tguedes2021@users.noreply.github.com>
2022-10-11 15:32:15 +0100

    HalfWay
    just added it to recognise the controller

--- Assets/Old Game/Scripts/Gun.cs
+++ Assets/Old Game/Scripts/Gun.cs
@@ -15,10 +15,10 @@ public class Gun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        /*if (Input.GetButtonDown("Fire1"))
         {
             Shoot();
-        }
+        }*/
     }
 
     void Shoot()
