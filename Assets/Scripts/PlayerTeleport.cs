5148152691055b2ae59b08785ccd27ccb69adebd
MaxElam2021 <94044736+MaxElam2021@users.noreply.github.com>
2022-10-18 15:29:06 +0100

    Credits Scene added

--- Assets/Scripts/PlayerTeleport.cs
+++ Assets/Scripts/PlayerTeleport.cs
@@ -13,6 +13,11 @@ public class PlayerTeleport : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+     
     }
+
+	void OnTriggerEnter(Collider Col)
+	{
+		Col.transform.position = new Vector3 (Random.Range (-50.0f, 50.0f), Col.transform.position.y, Random.Range(-50.0f, 50.0f)); 
+	}
 }
